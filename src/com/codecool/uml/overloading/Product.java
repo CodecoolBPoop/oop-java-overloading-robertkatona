2209b4fb84bf4a30d4b966ba0c3ca00005a83ec1
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-07-28 08:54:19 +0200

    product class initialized and id increases automaitcally

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -1,4 +1,87 @@
 package com.codecool.uml.overloading;
 
+import java.util.Currency;
+import java.util.List;
+import java.util.Currency;
+
 public class Product {
+
+    private static int counter = 1;
+    private int id;
+    private String name;
+    private float defaultPrice;
+    private Currency defaultCurrency;
+    private ProductCategory productCategory;
+    private Supplier supplier;
+    private List<Product> productList;
+
+    public Product() {
+        this.id = counter ++;
+    }
+
+    public Product(String name, float defaultPrice, Currency defaultCurrency) {
+        this.name = name;
+        this.defaultPrice = defaultPrice;
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDefaultPrice() {
+        return defaultPrice;
+    }
+
+    public void setDefaultPrice(float defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public Currency getDefaultCurrency() {
+        return defaultCurrency;
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public List<Product> getAllProductsBy(ProductCategory productCategory){
+        List<Product> productListByPoduct= this.productList;
+        return productListByPoduct;
+    }
+
+    public List<Product> getAllProductsBy(Supplier supplier){
+        List<Product> productListBySupplier= this.productList;
+        return productListBySupplier;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
+
