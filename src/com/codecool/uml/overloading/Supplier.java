6345868238a6ba3c470244e938af4c63ddaf6276
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-07-30 07:12:15 +0200

    every product has a supplier

--- src/com/codecool/uml/overloading/Supplier.java
+++ src/com/codecool/uml/overloading/Supplier.java
@@ -1,4 +1,32 @@
 package com.codecool.uml.overloading;
 
+import java.util.List;
+
 public class Supplier {
+
+    private static int id;
+    private String name;
+    private String description;
+
+    public Supplier(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
